519639613877c423e6e6de17f1191755426fbb03
mona lisa <jonjonamayastm@gmail.com>
2023-04-25 14:42:30 -0700

    finsihed README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Password Generator Starter Code
+# Password Generator / Challenge 3
+When user opens web page, they click the "generate password" button
+Which then deploys pop ups with options for user to select to create password
+The application generates a password based on criteria user had selected
+
+### Screenshot of Deployed App
+![Screenshot](assets/images/pwordgenscreenshot.png)
+
+## Links
+[DeployedApp] (https://jmarkamaya.github.io/Challenge3-Password-Generator/)
+
+
--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");
 var lowerChars = ['a', 'b', 'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','w','x','y','z']
 var upperChars = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 var numericChars = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-var specialChars = ["!", "@", "#","$","%","^","&","*","_","="]
+var specialChars = ["~" ,"!", "@", "#", "$", "%", "^", "&", "*", "_", "=", "/","?"]
 var options = []
 var length; 
 // user gives options
